dab4927da67f05dfcde03a8bdbdff3675afe26d5
Neeraj Bansal <neeraj.bansal@hpe.com>
2018-03-25 14:43:06 +0530

    initial commmit

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,174 @@
+import React from 'react';
+
+import { getColor } from '../utils/colors';
+
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  Badge,
+  Button,
+} from 'reactstrap';
+
+import {
+  MdInsertChart,
+  MdBubbleChart,
+  MdPieChart,
+  MdShowChart,
+  MdPersonPin,
+} from 'react-icons/lib/md';
+
+import InfiniteCalendar from 'react-infinite-calendar';
+
+import { Line, Bar } from 'react-chartjs-2';
+
+import {
+  supportTicketsData,
+  productsData,
+  userProgressTableData,
+  todosData,
+  chartjs,
+} from '../demos/dashboardPage';
+import { getStackLineChart, stackLineChartOptions } from '../demos/chartjs';
+
+import Page from '../components/Page';
+
+import SupportTicket from '../components/SupportTicket';
+import ProductMedia from '../components/ProductMedia';
+import UserProgressTable from '../components/UserProgressTable';
+
+import { AnnouncementCard, TodosCard } from '../components/Card';
+
+import { NumberWidget } from '../components/Widget';
+
+import MapWithBubbles from '../components/MapWithBubbles';
+
+const today = new Date();
+const lastWeek = new Date(
+  today.getFullYear(),
+  today.getMonth(),
+  today.getDate() - 7
+);
+
+class DashboardPage extends React.Component {
+  componentDidMount() {
+    // this is needed, because InfiniteCalendar forces window scroll
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    const primaryColor = getColor('primary');
+    const secondaryColor = getColor('secondary');
+
+    return (
+      <Page
+        className="DashboardPage"
+        title="Dashboard"
+        breadcrumbs={[{ name: 'Dashboard', active: true }]}
+      >
+        <Row>
+          <Col lg={3} md={6} sm={6} xs={12}>
+            <NumberWidget
+              title="Total Profit"
+              subtitle="This month"
+              number="9.8k"
+              color="secondary"
+              progress={{
+                value: 75,
+                label: 'Last month',
+              }}
+            />
+          </Col>
+
+          <Col lg={3} md={6} sm={6} xs={12}>
+            <NumberWidget
+              title="Monthly Visitors"
+              subtitle="This month"
+
+              color="secondary"
+              progress={{
+                value: 45,
+                label: 'Last month',
+              }}
+            />
+          </Col>
+
+          <Col lg={3} md={6} sm={6} xs={12}>
+            <NumberWidget
+              title="New Users"
+              subtitle="This month"
+              number="3,400"
+              color="secondary"
+              progress={{
+                value: 90,
+                label: 'Last month',
+              }}
+            />
+          </Col>
+
+          <Col lg={3} md={6} sm={6} xs={12}>
+            <NumberWidget
+              title="Bounce Rate"
+              subtitle="This month"
+              number="38%"
+              color="secondary"
+              progress={{
+                value: 60,
+                label: 'Last month',
+              }}
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col lg="8" md="12" sm="12" xs="12">
+            <Card>
+              <CardHeader>
+                Total Revenue{' '}
+                <small className="text-muted text-capitalize">This year</small>
+              </CardHeader>
+              <CardBody>
+                <Line data={chartjs.line.data} options={chartjs.line.options} />
+              </CardBody>
+            </Card>
+          </Col>
+
+          <Col lg="4" md="12" sm="12" xs="12">
+            <Card>
+              <CardHeader>Total Expense</CardHeader>
+              <CardBody>
+                <Bar data={chartjs.bar.data} options={chartjs.bar.options} />
+              </CardBody>
+              <ListGroup flush>
+                <ListGroupItem>
+                  <MdInsertChart size={25} color={primaryColor} /> Cost of sales{' '}
+                  <Badge color="secondary">$3000</Badge>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <MdBubbleChart size={25} color={primaryColor} /> Management
+                  costs <Badge color="secondary">$1200</Badge>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <MdShowChart size={25} color={primaryColor} /> Financial costs{' '}
+                  <Badge color="secondary">$800</Badge>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <MdPieChart size={25} color={primaryColor} /> Other operating
+                  costs <Badge color="secondary">$2400</Badge>
+                </ListGroupItem>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+
+
+      </Page>
+    );
+  }
+}
+export default DashboardPage;
